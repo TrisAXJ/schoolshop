3fbee54ed9b19f38287570b041baf0ea187a802f
TrisA <1628791325@qq.com>
2022-04-22 15:41:41 +0800

    改善Bug

--- sss.py
+++ sss.py
@@ -11,7 +11,7 @@ import datetime
 import requests
 import time
 from jsonpath import jsonpath
-from past.builtins import raw_input
+# from past.builtins import raw_input
 from win10toast_click import ToastNotifier
 
 
@@ -37,26 +37,26 @@ def date():
     }
 
     trys = 0
-    while trys <= 3:
+    while True:
         # POST请求
         Response = requests.post('https://yun.yun8609.net//wxmall/wmall/getCateInfoList', headers=headers, data=data)
         if Response.status_code == 200:  # 成功
             trys = 0
-            print(">>"+ str(datetime.datetime.now())+",请求成功！<<")
+            # print(">>"+ str(datetime.datetime.now())+",请求成功！<<")
             return Response
         else:  # 失败重试3次
             print(">>"+ str(datetime.datetime.now())+"<<"+"\n>>Error request:" + str(Response.status_code)+"<<")
             trys = trys + 1
-            if trys == 4:
-                time.sleep(0.5)
-                break
+            # if trys == 4:
+            #     time.sleep(0.5)
+            #     break
             time.sleep(1)
-            print(">>Retry after 60s later...")
-            time.sleep(60)
+            print(">>Retry after 30s later...")
+            time.sleep(30)
             print(">>>>重新请求接口...第%s次<<<<" % trys)
             time.sleep(0.5)
-    print(">>>>请求错误，请重启程序！<<<<")
-    raw_input("Press <enter>")  # 防止直接关闭窗口
+    # print(">>>>请求错误，请重启程序！<<<<")
+    # raw_input("Press <enter>")  # 防止直接关闭窗口
 
     # respose = Response
     # return respose
@@ -64,16 +64,16 @@ def date():
 
 def solve_date():
     res = date().json()
-    count = jsonpath(res, "$..count")
     th_name = jsonpath(res, "$..name")
+    count = len(th_name)
     th_num = jsonpath(res, "$..stockqty")
     th_price = jsonpath(res, "$..mallmprice")
     all = []
     log_solve = 1
 
     # 响应中 count代表列表中商品数量 若商品列表无数据 count为0
-    if count != [0]:
-        for i in range(0, count[0]):
+    if count != 0:
+        for i in range(0, count):
             if th_num[i] <= 0:
                 th_num[i] = 0
                 all.append([th_name[i], th_num[i], th_price[i]])
@@ -90,18 +90,21 @@ def msgbox():
     msg = all
     msgg = ""
     if log_solve == 1:  # 如果商品列表有数据
-        for i in range(0, num[0]):
+        for i in range(0, num):
             if msg[i][1] == 0:
                 msg[i].append("❌已售罄")
             else:
                 msg[i].append("✅在售中")
 
-            msgg = msgg + msg[i][0] + str(msg[i][2]) + "元,剩余" + " [" + str(msg[i][1]) + "]  " + msg[i][3] + "\n"
+            msgg = msgg + ">>" + msg[i][0] + str(msg[i][2]) + "元\n剩余" + " [" + str(msg[i][1]) + "]  " + msg[i][3] + "\n"
     else:
-        msgg = "商店商品列表为空"
+        msgg = "商店商品已全部下架"
 
     total = str(datetime.datetime.now()) + "\n" + msgg
     print(total)  # 程序窗口中输出
+
+    # pushiplus推送
+    # sendmessage(total)
 
     # 桌面通知 已知Bug: py程序封装成exe程序后，show_toast中icon无法调用报错 不影响程序运行
     toaster = ToastNotifier()
@@ -109,37 +112,61 @@ def msgbox():
     time.sleep(0.5)
     print("持续监控中...")
 
+# pushplus 推送
+# def sendmessage(total):
+#     token = ''  # 需要更改你的token
+#     title = '商品变更通知'  # 改成你要的标题内容
+#     content = total  # 改成你要的正文内容
+#     topic = ''  # 群组编码，可以进行群发，需更改url
+#     url = 'http://www.pushplus.plus/send?token='+token+'&title='+title+'&content='+content+'&topic='+topic
+#     requests.get(url)
+#     time.sleep(0.5)
 
 def main():
     his_all, his_num, his_no = solve_date()  # 先发次请求记录商品列表信息，用作后续判断触发消息通知条件
     print(">>>>正在监控商品列表.......")
+    print("目前商品列表:\n")
+    msgbox()
     time.sleep(3)
     while True:  # 本demo使用死循环不太建议正规项目使用 建议使用schedule Task
-        all, num, log_solve = solve_date()  # 获取信息 all为处理过的列表[['商品名','库存','价格','状态'],[],...];num为商品列表中商品数量;
+        all, num, log_solve = solve_date()  # 获取信息 all为处理过的列表[['商品名','库存','价格'],[],...];num为商品列表中商品种类数量;
         # msgbox()
         if len(his_all) != 0 and len(all) != 0:  # 旧商品列表与新商品列表都有数据
-            for i in range(0, num[0]):
-                if his_num == num:
+            if his_num == num:
+                key = 0
+                for i in range(0, num):
                     if his_all[i][0] != all[i][0]:  # 若商品列表变化触发消息
-                        msgbox()
-                        his_all = all
-                        time.sleep(5)
-                    elif int(his_all[i][1]) - int(all[i][1]) > 0 and all[i][1] == 0: # 库存为0 触发消息
-                        msgbox()
-                        his_all = all
-                        time.sleep(5)
-                elif his_num != num: # 历史与新 商品数不同 触发消息
+                        key = key + 1
+                        # his_all = all
+                    elif int(his_all[i][1]) - int(all[i][1]) > 0 and all[i][1] == 0:  # 库存为0 触发消息
+                        key = key + 1
+                        # his_all = all
+                    else:  # 商品列表未发生变化
+                        key = 0
+
+                if key != 0:
                     msgbox()
                     his_all = all
                     time.sleep(5)
-        elif len(his_all) == 0 and len(all) != 0: #旧列表为空 新列表有数据 触发消息
+                else:
+                    # print('商品列表未发生变化')
+                    his_all = all
+                    time.sleep(10)
+            elif his_num != num:  # 历史与新 商品数不同 触发消息
+                msgbox()
+                his_all = all
+                his_num = num
+                time.sleep(5)
+        elif len(his_all) == 0 and len(all) != 0:  # 旧列表为空 新列表有数据 触发消息
             print(">>>>商店已开门！")
+            msgbox()
+            his_all = all
             time.sleep(5)
         elif len(all) == 0 and len(his_all) != 0:  # 新商品列表为空 触发消息
             msgbox()
             his_all = all
             time.sleep(5)
-        elif len(his_all) == 0 and len(all) == 0: # 新旧商品列表均为空
+        elif len(his_all) == 0 and len(all) == 0:  # 新旧商品列表均为空
             time.sleep(10)
             continue
 
@@ -149,3 +176,79 @@ if __name__ == '__main__':
     # solve_date()
     # msgbox()
     main()  # 入口
+
+    '''
+    while True:  # 本demo使用死循环不太建议正规项目使用 建议使用schedule Task
+        all, num, log_solve = solve_date()  # 获取信息 all为处理过的列表[['商品名','库存','价格','状态'],[],...];num为商品列表中商品种类数量;
+        # msgbox()
+        if len(his_all) != 0 and len(all) != 0:  # 旧商品列表与新商品列表都有数据
+            if his_num == num:
+                for i in range(0, num[0]):
+                    key = 0
+                    if his_all[i][0] != all[i][0]:  # 若商品列表变化触发消息
+                        key = key + 1
+                        his_all = all
+                        time.sleep(5)
+                    elif int(his_all[i][1]) - int(all[i][1]) > 0 and all[i][1] == 0: # 库存为0 触发消息
+                        key = key + 1
+                        his_all = all
+                        time.sleep(5)
+                    else:   # 商品列表未发生变化
+                        time.sleep(10)
+                if key != 0 :
+                    msgbox()
+            elif his_num != num: # 历史与新 商品数不同 触发消息
+                msgbox()
+                his_all = all
+                time.sleep(5)
+        elif len(his_all) == 0 and len(all) != 0: #旧列表为空 新列表有数据 触发消息
+            print(">>>>商店已开门！")
+            msgbox()
+            his_all = all
+            time.sleep(5)
+        elif len(all) == 0 and len(his_all) != 0:  # 新商品列表为空 触发消息
+            msgbox()
+            his_all = all
+            time.sleep(5)
+        elif len(his_all) == 0 and len(all) == 0: # 新旧商品列表均为空
+            time.sleep(10)
+            continue
+        '''
+    '''    while True:  # 本demo使用死循环不太建议正规项目使用 建议使用schedule Task
+        all, num, log_solve = solve_date()  # 获取信息 all为处理过的列表[['商品名','库存','价格','状态'],[],...];num为商品列表中商品种类数量;
+        # msgbox()
+        if len(his_all) != 0 and len(all) != 0:  # 旧商品列表与新商品列表都有数据
+            if his_num == num:
+                for i in range(0, num[0]):
+                    if his_all[i][0] != all[i][0]:  # 若触发消息
+                        print("商品列表变化")
+                        msgbox()
+                        his_all = all
+                        time.sleep(5)
+                    elif int(his_all[i][1]) - int(all[i][1]) > 0 and all[i][1] == 0: # 库存为0 触发消息
+                        print("库存为0 触发消息")
+                        msgbox()
+                        his_all = all
+                        time.sleep(5)
+                    else:   # 商品列表未发生变化
+                        print("商品列表未发生变化")
+                        his_all = all
+                        time.sleep(5)
+            elif his_num != num: # 历史与新 商品数不同 触发消息
+                print("历史与新 商品数不同")
+                msgbox()
+                his_all = all
+                time.sleep(5)
+        elif len(his_all) == 0 and len(all) != 0: #旧列表为空 新列表有数据 触发消息
+            print(">>>>商店已开门！")
+            msgbox()
+            his_all = all
+            time.sleep(5)
+        elif len(all) == 0 and len(his_all) != 0:  # 新商品列表为空 触发消息
+            print("新商品列表为空 触发消息")
+            msgbox()
+            his_all = all
+            time.sleep(5)
+        elif len(his_all) == 0 and len(all) == 0: # 新旧商品列表均为空
+            time.sleep(10)
+            continue'''
